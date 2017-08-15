30ee17191fcd5e0faf9f3fd428dbf0b1edcdc14d
monica lai <monju4177@gmail.com>
2017-08-15 13:38:54 +0800

    Added devise views

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   get 'tickets/show'
 
+  root to: 'pages#index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
