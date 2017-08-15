12276892225877d2ae8c144be57e604f4a8636ea
Yuqing Yang <rachely.0208@gmail.com>
2017-08-15 01:50:19 -0700

    customer id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815064600) do
+ActiveRecord::Schema.define(version: 20170815084001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170815064600) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "customer_id"
     t.index ["author_id"], name: "index_tickets_on_author_id"
   end
 
