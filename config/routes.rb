5bdd355f4c1efe25a003e80bef44c568f05bd42e
Yuqing Yang <rachely.0208@gmail.com>
2017-08-15 00:52:38 -0700

    :routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  resources :tickets
 
-  get 'tickets/edit'
-
-  get 'tickets/new'
-
-  get 'tickets/create'
-
-  get 'tickets/update'
-
-  get 'tickets/destroy'
-
-  get 'tickets/index'
-
-  get 'tickets/show'
-
-  root to: 'pages#index'
+  root to: tickets_path
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
