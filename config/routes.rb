573c1af7ea109571dc19dc93cbd59b724f4efb28
Yuqing Yang <rachely.0208@gmail.com>
2017-08-15 00:56:25 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :tickets
 
-  root to: tickets_path
+  root to: "tickets#index"
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
