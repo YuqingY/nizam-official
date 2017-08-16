433a032f8271a1daff84899bffc0ac5d9ca95e34
Zoey Tang <zt72@cornell.edu>
2017-08-16 13:30:48 +0800

    policy scope done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :tickets
 
   root to: "tickets#index"
-
+  # Controller#action
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
