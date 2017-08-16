18bac58fa7767f503c84981f7967608763e92bc0
Yuqing Yang <rachely.0208@gmail.com>
2017-08-15 23:18:44 -0700

    log ut bug fixed

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,14 @@ class Ticket < ApplicationRecord
   belongs_to :author, class_name: User
   belongs_to :assignee, class_name: User, required: false
   belongs_to :assigner, class_name: User, required: false
+
+  scope :active_ticket, -> (){ where(status: "active") }
+  scope :new_ticket, -> (){ where(status: "new") }
+  scope :pending_ticket, -> (){ where(status: "pending") }
+  scope :ready_ticket, -> (){ where(status: "ready") }
+  scope :closed_ticket, -> (){ where(status: "closed") }
+
   # def self.active
-  #   .select
+  #   Ticket.all.select { |x| x.status == "active"}
   # end
 end
