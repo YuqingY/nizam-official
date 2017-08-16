4b9863e12388196e8058325b1ec377e715e5f616
Yuqing Yang <rachely.0208@gmail.com>
2017-08-15 21:23:37 -0700

    fix bug

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,7 @@ class Ticket < ApplicationRecord
   belongs_to :author, class_name: User
   belongs_to :assignee, class_name: User, required: false
   belongs_to :assigner, class_name: User, required: false
+  # def self.active
+  #   .select
+  # end
 end
