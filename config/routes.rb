ef2255de494232e61b0500f937c175f40c401757
Yuqing Yang <rachely.0208@gmail.com>
2017-08-16 23:24:32 -0700

    authen fixed

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root to: "tickets#index"
   # Controller#action
   devise_for :users
+
+  get 'new_call', to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
