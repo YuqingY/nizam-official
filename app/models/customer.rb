7b2a10f5ab2968ab94a35827e8ada941f116750a
Yuqing Yang <rachely.0208@gmail.com>
2017-08-16 20:57:23 -0700

    ticket model, new seed, validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  validates :cnic, uniqueness: true
+  validates :cnic, presence: true
+  has_many :tickets
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816092629) do
+ActiveRecord::Schema.define(version: 20170817030234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,8 +43,9 @@ ActiveRecord::Schema.define(version: 20170816092629) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "customer_id"
+    t.bigint "customer_id"
     t.index ["author_id"], name: "index_tickets_on_author_id"
+    t.index ["customer_id"], name: "index_tickets_on_customer_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,10 +63,12 @@ ActiveRecord::Schema.define(version: 20170816092629) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "calls", "tickets"
+  add_foreign_key "tickets", "customers"
   add_foreign_key "tickets", "users", column: "author_id"
 end
